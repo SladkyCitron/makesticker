8599fb9bff6e249b850b84087ad2eb31b5e89cd9
Matúš Ollah <matusollah420@gmail.com>
2025-05-01 19:40:35 +0200

    add character list

---
+++ scripts/gen_char_table.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	assetsFlag := flag.String("assets", "assets", "Path to assets directory")
+	outputFlag := flag.String("output", "characters.md", "Output file")
+	flag.Parse()
+
+	var mdBuf bytes.Buffer
+
+	fmt.Fprintln(&mdBuf, "# List of characters")
+	fmt.Fprintln(&mdBuf)
+	fmt.Fprintln(&mdBuf, "| Name | Sample image |")
+	fmt.Fprintln(&mdBuf, "|------|--------------|")
+
+	err := filepath.WalkDir(filepath.Join(*assetsFlag, "characters"), func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		fmt.Fprintf(&mdBuf, "| %[1]s | <img src=\"assets/characters/%[1]s.png\" alt=\"%[1]s\" width=\"128\"> |\n", strings.ReplaceAll(filepath.Base(path), ".png", ""))
+
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	if err := os.WriteFile(*outputFlag, mdBuf.Bytes(), 0o666); err != nil {
+		panic(err)
+	}
+}
